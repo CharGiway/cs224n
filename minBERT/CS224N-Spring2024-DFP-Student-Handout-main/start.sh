9cb2111810d7e29cc04f92217e9ba923b0e8eefd
caizhw3 <caizhw3@gmail.com>
2025-08-30 17:36:40 +0000

    fix

--- minBERT/CS224N-Spring2024-DFP-Student-Handout-main/start.sh
+++ minBERT/CS224N-Spring2024-DFP-Student-Handout-main/start.sh
@@ -2,6 +2,6 @@
 set -e
 export HF_ENDPOINT="https://hf-mirror.com"
 
-python3 classifier.py --fine-tune-mode full-model --lr 1e-5 --use_gpu
+# python3 classifier.py --fine-tune-mode full-model --lr 1e-5 --use_gpu
 
-# python3 classifier.py --fine-tune-mode last-linear-layer --lr 1e-3
+python3 classifier.py --fine-tune-mode last-linear-layer --lr 1e-3 --use_gpu
